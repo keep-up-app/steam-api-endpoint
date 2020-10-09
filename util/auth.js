384873f679211a81658b4ddc45250feec34f9078
Noah Greff <greffnoah@gmail.com>
2020-10-09 14:59:30 +0200

    adding auth.js

--- util/auth.js
+++ util/auth.js
@@ -9,7 +9,7 @@ const SteamAuth = require("node-steam-openid");
  */
 
 module.exports = new SteamAuth({
-    realm: `${process.env.BASE_URL}/steam/user/auth`,
-    returnUrl: `${process.env.BASE_URL}/steam/user/auth`,
-    apiKey: process.env.STEAM_API_KEY
+    realm: "",
+    returnUrl: "",
+    apiKey: ""
 });
