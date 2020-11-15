a9ea6b4755f970ab902300618b04bca95ce7ebf7
Noah Greff <greffnoah@gmail.com>
2020-11-15 21:30:34 +0100

    changing returnURL from steam auth

--- util/auth.js
+++ util/auth.js
@@ -10,6 +10,6 @@ const SteamAuth = require("node-steam-openid");
 
 module.exports = new SteamAuth({
     realm: `${process.env.BASE_URL}/steam/user/auth`,
-    returnUrl: `${process.env.BASE_URL}/steam/user/auth`,
+    returnUrl: `${process.env.BASE_URL}/steam/user/authenticated`,
     apiKey: process.env.STEAM_API_KEY
 });
